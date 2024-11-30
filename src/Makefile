e52b18442976f63864e316d66559a9d3e4f3721e
Racovcen Laurentiu <118909961+laurentiu-racovcen@users.noreply.github.com>
2024-11-30 22:22:00 +0200

    Initial commit

---
+++ src/Makefile
@@ -0,0 +1,4 @@
+build:
+		g++ main.cpp -Wall -Werror -o tema1 -lpthread
+clean:
+		rm tema1
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <pthread.h>
+
+int main(int argc, char **argv)
+{
+    printf("tema1a");
+
+    return 0;
+}
