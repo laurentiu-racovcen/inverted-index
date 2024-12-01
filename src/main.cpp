e9c96355c65e2283566fa42b0dc23fcdc08395e8
Racovcen Laurentiu <118909961+laurentiu-racovcen@users.noreply.github.com>
2024-12-01 22:26:07 +0200

    Started mapper function implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,86 @@
 #include <iostream>
+#include <map>
+#include <vector>
+#include <queue>
 #include <fstream>
 #include <pthread.h>
 
+#include "threadpool.h"
+
 using namespace std;
+
+void *mapper_function(void *arg) {
+    if (arg == NULL) {
+        cout << "Argumentul pentru mapper function nu este valid!";
+        exit(-1);
+    }
+    threadpool_t *tp = (threadpool_t *) arg;
+    // procesarea fisierelor disponibile in coada
+    while (true) {
+        pthread_mutex_lock(&tp->work_mutex);
+
+        // daca e setat flagul de "finished_mapping", thread-ul se opreste
+        if (tp->finished_mapping) {
+            pthread_mutex_unlock(&tp->work_mutex);
+            pthread_exit(NULL);
+        }
+
+        // in coada nu mai sunt fisiere de procesat
+        if (tp->files_queue.empty()) {
+            tp->finished_mapping = true;
+            pthread_mutex_unlock(&tp->work_mutex);
+            pthread_exit(NULL);
+        }
+
+        // extrage din coada primul fisier disponibil
+        file_info_t file_info = tp->files_queue.front();
+        tp->files_queue.pop();
+
+        pthread_mutex_unlock(&tp->work_mutex);
+
+        // proceseaza fisierul primit din coada
+        ifstream q_file(file_info.file_path);
+        if (!q_file) {
+            cerr << "\nNu a putut fi deschis fisierul " << file_info.file_path << " !\n\n";
+        }
+
+        cout << "Fisierul primit este: " << file_info.file_path << "\n Continutul fisierului:\n";
+
+        string test_line;
+        while (getline(q_file, test_line)) {
+            cout << test_line << "\n";
+        }
+
+        // inchidere fisier
+        q_file.close();
+    }
+}
+
+void *reducer_function(void *arg) {
+    threadpool_t *tp = (threadpool_t *) arg;
+    if (tp == NULL) {
+        cout << "Argumentul pentru reducer function nu este valid!";
+        exit(-1);
+    }
+
+    // procesare task-uri
+    while (true) {
+        pthread_mutex_lock(&tp->work_mutex);
+
+        // daca mapperii au terminat lucrul
+        if (tp->finished_mapping) {
+            // daca nu mai exista de lucru in coada, thread-ul isi termina executia
+            if (tp->finished_reducing) {
+                pthread_mutex_unlock(&tp->work_mutex);
+                pthread_exit(NULL);
+            }
+
+            // TODO: reducer thread work
+        }
+
+        pthread_mutex_unlock(&tp->work_mutex);
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -37,8 +115,8 @@ int main(int argc, char **argv)
     }
 
     // se deschide fisierul, daca este valid
-    ifstream input_file(argv[3]);
-    if (!input_file) {
+    ifstream fin(argv[3]);
+    if (!fin) {
         cerr << "\nNu a putut fi deschis fisierul!\n\n";
         exit(3);
     }
@@ -47,7 +125,79 @@ int main(int argc, char **argv)
     cout << "Numarul de reducer threads: " << num_reducer_threads << "\n";
     cout << "Fisierul citit: " << argv[3] << "\n";
 
-    // pthread_t threads[];
+    // initializare threadpool
+    threadpool_t tp;
+    tp.finished_mapping = false;
+    tp.finished_reducing = false;
+    pthread_mutex_init(&(tp.work_mutex), NULL);
+    tp.threads = (pthread_t*) calloc(num_mapper_threads + num_reducer_threads, sizeof(pthread_t));
+    tp.partial_lists = (map<string, vector<unsigned int>>*) calloc(num_mapper_threads, sizeof(map<string, vector<unsigned int>>));
+
+    // citire numar fisiere
+    string line;
+    getline(fin, line);
+    unsigned long files_num = stoul(line);
+
+    cout << "Numarul de fisiere: " << files_num << "\n";
+    
+    for (size_t i = 1; getline(fin, line); i++) {
+        cout << "se baga in coada linia: " << line << ", id=" << i << "\n";
+        file_info_t file_info;
+        file_info.file_id = i;
+        file_info.file_path = line;
+        tp.files_queue.push(file_info);
+    }
+
+    // while (!tp.files_queue.empty()) {
+       
+    //     // getting front element of queue
+    //     cout << tp.files_queue.front().file_path << " ";
+       
+    //     // removing front element of queue
+    //     tp.files_queue.pop();
+    // }
+
+    // se initializeaza thread-urile "mapper"
+    for (long int i = 0; i < num_mapper_threads; i++) {
+        int ret = pthread_create(&tp.threads[i], NULL, mapper_function, &tp);
+        if (ret) {
+            cout << "Eroare la crearea thread-ului " << i << "\n";
+            exit(-1);
+        }
+    }
+
+    
+    
+
+    // se initializeaza thread-urile "reducer"
+    // for (long int i = num_mapper_threads; i < num_mapper_threads + num_reducer_threads; i++) {
+    //     int ret = pthread_create(&tp.threads[i], NULL, reducer_function, &tp);
+    //     if (ret) {
+    //         cout << "Eroare la crearea thread-ului " << i << "\n";
+    //         exit(-1);
+    //     }
+    // }
+
+    cout << "Se asteapta toate thread-urile...\n";
+
+    // se asteapta toate thread-urile
+    for (int i = 0; i < num_mapper_threads; i++) {
+        void *status;
+        int ret = pthread_join(tp.threads[i], &status);
+        if (ret) {
+            cout << "Eroare la asteptarea thread-ului " << i << "\n";
+            exit(-1);
+        }
+    }
+
+    // exit(10);
+
+    // distrugere mutex din threadpool
+    pthread_mutex_destroy(&(tp.work_mutex));
+
+    // se elibereaza memoria alocata pentru thread-uri si pentru liste
+    free(tp.threads);
+    free(tp.partial_lists);
 
     return 0;
 }
---
+++ src/threadpool.h
@@ -0,0 +1,28 @@
+#ifndef __THREADPOOL_H__
+#define __THREADPOOL_H__
+
+#include <iostream>
+#include <pthread.h>
+#include <string>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+typedef struct {
+    unsigned int file_id;
+    string file_path;
+} file_info_t;
+
+typedef struct {
+    unsigned int num_threads;
+    pthread_t *threads;
+    pthread_mutex_t work_mutex;        // pentru sincronizarea operatiilor efectuate de thread-uri
+    queue<file_info_t> files_queue;   // stocheaza datele fisierelor care trebuie procesate de mapperi
+    pthread_cond_t cond;
+    map<string, vector<unsigned int>> *partial_lists; // o lista partiala contine elemente de forma (cuvant, lista de file_id-uri)
+    bool finished_mapping;            // pentru a semnaliza daca procesarea de catre mapperi s-a terminat (coada de task-uri "map" este goala)
+    bool finished_reducing;           // pentru a semnaliza daca procesarea de catre reduceri s-a terminat (coada de task-uri "reduce" este goala)
+} threadpool_t;
+
+#endif // __THREADPOOL_H__
