37af9413855a9134b52d00d0b9b6e56fac656d6b
Racovcen Laurentiu <118909961+laurentiu-racovcen@users.noreply.github.com>
2024-12-02 18:10:25 +0200

    Implemented mappers function

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,69 @@
 #include <queue>
 #include <fstream>
 #include <pthread.h>
+#include <sstream>
 
 #include "threadpool.h"
 
 using namespace std;
+
+// Functie care transforma cuvintele astfel incat acestea sa contina
+// doar caractere alfabetice
+string normalize_word(string word) {
+    for (size_t i = 0; i < word.size(); i++) {
+        if ((word[i] >= 'A') && (word[i] <= 'Z')) {
+            // transforma caracterele uppercase in caractere lowercase
+            word[i] += 32;
+        } else if (!(((word[i] >= 'A') && (word[i] <= 'Z')) ||
+                    ((word[i] >= 'a') && (word[i] <= 'z')))) {
+            // stergere caractere neconforme (diferite de cele alfabetice)
+            word.erase(word.begin() + i);
+        }
+    }
+    return word;
+}
+
+// Functie care extrage toate cuvintele unice dintr-un fisier
+void extract_words_from_file(ifstream& input, unsigned int file_id, map<string, vector<unsigned int>> *partial_list) {
+    string current_line;
+    while (getline(input, current_line)) {
+        string word;
+        stringstream words(current_line);
+        while (words >> word) {
+            string normalized_word = normalize_word(word);
+            // verifica daca cuvantul se afla in lista partiala
+            if (partial_list->find(normalized_word) == partial_list->end()) {
+                // adauga cuvantul si file_id-ul primit in lista
+                vector<unsigned int> file_ids;
+                file_ids.push_back(file_id);
+                partial_list->insert(make_pair(normalized_word, file_ids));
+            } else {
+                // lista de file_id-uri nu este goala;
+                // daca file_id-ul curent nu exista in lista de file_id-uri ale cuvantului, acesta se adauga
+                bool found_id = false;
+                for (auto id : partial_list->at(normalized_word)) {
+                    if (id == file_id) found_id = true;
+                }
+                if (!found_id) {
+                    partial_list->at(normalized_word).push_back(file_id);
+                    // cout << "found new file id: " << file_id << "\n";
+                    // cout << "list of file ids of '" << normalized_word << "': ";
+                    // for (auto id : partial_list->at(normalized_word)) {
+                    //     cout << id << ", ";
+                    // }
+                    // cout << endl;
+                }
+            }
+        }
+        // cout << "file id=" << file_id << ", line=" << current_line << "\n";
+    }
+
+    // cout << "\n Cuvintele fisierului curent:\n";
+
+    // for (auto i : partial_list->elems) {
+    //     cout << i.first << ", ";
+    // }
+}
 
 void *mapper_function(void *arg) {
     cout << "New thread:\n";
@@ -15,7 +74,12 @@ void *mapper_function(void *arg) {
         cout << "Argumentul pentru mapper function nu este valid!";
         exit(-1);
     }
-    threadpool_t *tp = (threadpool_t *) arg;
+    // extragere date din thread_struct
+    thread_struct_t *thread_struct = (thread_struct_t *) arg;
+    threadpool_t *tp = (threadpool_t *) thread_struct->threadpool;
+    unsigned int thread_id = thread_struct->thread_id;
+
+    map<string, vector<unsigned int>> partial_list;
     // procesarea fisierelor disponibile in coada
     while (true) {
         pthread_mutex_lock(&tp->work_mutex);
@@ -26,14 +90,16 @@ void *mapper_function(void *arg) {
             pthread_exit(NULL);
         }
 
-        // in coada nu mai sunt fisiere de procesat
+        // daca in coada nu mai sunt fisiere de procesat
         if (tp->files_queue.empty()) {
             tp->finished_mapping = true;
+            // lista partiala a thread-ului se copiaza in lista de liste partiale ale thread-urilor
+            tp->partial_lists[thread_id].elems = partial_list;
             pthread_mutex_unlock(&tp->work_mutex);
             pthread_exit(NULL);
         }
 
-        // extrage din coada primul fisier disponibil
+        // extrage din coada primul nod
         file_info_t file_info = tp->files_queue.front();
         tp->files_queue.pop();
 
@@ -45,14 +111,13 @@ void *mapper_function(void *arg) {
             cerr << "\nNu a putut fi deschis fisierul " << file_info.file_path << " !\n\n";
         }
 
-        cout << "Fisierul primit este: " << file_info.file_path << "\n Continutul fisierului:\n";
+        // cout << "Fisierul primit este: " << file_info.file_path;
 
-        string current_line;
-        while (getline(q_file, current_line)) {
-            cout << current_line << "\n";
-        }
-
-        extract_words_from_file(q_file, &(tp->partial_lists[]));
+        // lista partial_lists[i] contine lista partiala corespunzatoare thread-ului cu id-ul "i"
+        // initializarea listei partiale
+        tp->partial_lists[thread_id].is_visited_by_reducer = false;
+        cout << "file id=" << file_info.file_id;
+        extract_words_from_file(q_file, file_info.file_id, &partial_list);
 
         // inchidere fisier
         q_file.close();
@@ -92,8 +157,8 @@ void *reducer_function(void *arg) {
             // agregarea cuvintelor din listele partiale disponibile in lista thread-ului curent
 
             // cauta prima lista nevizitata
-            for (size_t i = 0; i < tp->partial_lists.size(); i++) {
-                if (!(tp->partial_lists[i].is_visited)) {
+            for (size_t i = 0; i < tp->num_mapper_threads; i++) {
+                if (!(tp->partial_lists[i].is_visited_by_reducer)) {
                     // TODO
                     // process given unvisited partial list and put it in the thread list
                 }
@@ -150,63 +215,56 @@ int main(int argc, char **argv)
     cout << "Numarul de reducer threads: " << num_reducer_threads << "\n";
     cout << "Fisierul citit: " << argv[3] << "\n";
 
-    // initializare threadpool
-    threadpool_t tp;
-    tp.finished_mapping = false;
-    tp.finished_reducing = false;
-    pthread_mutex_init(&(tp.work_mutex), NULL);
-    tp.threads = (pthread_t*) calloc(num_mapper_threads + num_reducer_threads, sizeof(pthread_t));
-    // tp.partial_lists = (map<string, vector<unsigned int>>*) calloc(num_mapper_threads, sizeof(map<string, vector<unsigned int>>));
-
     // citire numar fisiere
     string line;
     getline(fin, line);
     unsigned long files_num = stoul(line);
-
     cout << "Numarul de fisiere: " << files_num << "\n";
+
+    // initializare threadpool
+    threadpool_t tp;
+    tp.num_mapper_threads = num_mapper_threads;
+    tp.num_reducer_threads = num_reducer_threads;
+    tp.finished_mapping = false;
+    tp.finished_reducing = false;
+    pthread_mutex_init(&(tp.work_mutex), NULL);
+    tp.threads = (pthread_t*) calloc(num_mapper_threads + num_reducer_threads, sizeof(pthread_t));
+    tp.partial_lists = new partial_list_t[num_mapper_threads];
     
+    // adaugarea datelor fisierelor in coada
     for (size_t i = 1; getline(fin, line); i++) {
-        cout << "se baga in coada linia: " << line << ", id=" << i << "\n";
+        cout << "se baga in coada calea: " << line << ", id=" << i << "\n";
         file_info_t file_info;
         file_info.file_id = i;
         file_info.file_path = line;
         tp.files_queue.push(file_info);
     }
 
-    // while (!tp.files_queue.empty()) {
-       
-    //     // getting front element of queue
-    //     cout << tp.files_queue.front().file_path << " ";
-       
-    //     // removing front element of queue
-    //     tp.files_queue.pop();
-    // }
-
     // se initializeaza thread-urile "mapper"
     for (long int i = 0; i < num_mapper_threads; i++) {
-        int ret = pthread_create(&tp.threads[i], NULL, mapper_function, &tp);
+        thread_struct_t thread_struct;
+        thread_struct.thread_id = i;
+        thread_struct.threadpool = &tp;
+        int ret = pthread_create(&tp.threads[i], NULL, mapper_function, &thread_struct);
         if (ret) {
             cout << "Eroare la crearea thread-ului " << i << "\n";
             exit(-1);
         }
     }
 
-    
-    
-
     // se initializeaza thread-urile "reducer"
-    // for (long int i = num_mapper_threads; i < num_mapper_threads + num_reducer_threads; i++) {
-    //     int ret = pthread_create(&tp.threads[i], NULL, reducer_function, &tp);
-    //     if (ret) {
-    //         cout << "Eroare la crearea thread-ului " << i << "\n";
-    //         exit(-1);
-    //     }
-    // }
+    for (long int i = num_mapper_threads; i < num_mapper_threads + num_reducer_threads; i++) {
+        int ret = pthread_create(&tp.threads[i], NULL, reducer_function, &tp);
+        if (ret) {
+            cout << "Eroare la crearea thread-ului " << i << "\n";
+            exit(-1);
+        }
+    }
 
     cout << "Se asteapta toate thread-urile...\n";
 
     // se asteapta toate thread-urile
-    for (int i = 0; i < num_mapper_threads; i++) {
+    for (int i = 0; i < num_mapper_threads + num_reducer_threads; i++) {
         void *status;
         int ret = pthread_join(tp.threads[i], &status);
         if (ret) {
@@ -215,14 +273,12 @@ int main(int argc, char **argv)
         }
     }
 
-    // exit(10);
-
     // distrugere mutex din threadpool
     pthread_mutex_destroy(&(tp.work_mutex));
 
     // se elibereaza memoria alocata pentru thread-uri si pentru liste
     free(tp.threads);
-    // free(tp.partial_lists);
+    delete[] tp.partial_lists;
 
     return 0;
 }
--- src/threadpool.h
+++ src/threadpool.h
@@ -5,9 +5,14 @@
 #include <pthread.h>
 #include <string>
 #include <queue>
-#include <map>
+#include <set>
 
 using namespace std;
+
+typedef struct {
+    map<string, vector<unsigned int>> elems;
+    bool is_visited_by_reducer; // pentru sincronizarea thread-urilor reducer
+} partial_list_t;
 
 typedef struct {
     unsigned int file_id;
@@ -15,15 +20,21 @@ typedef struct {
 } file_info_t;
 
 typedef struct {
-    unsigned int num_threads;
+    unsigned int num_mapper_threads;
+    unsigned int num_reducer_threads;
     pthread_t *threads;
-    pthread_mutex_t work_mutex;        // pentru sincronizarea operatiilor efectuate de thread-uri
+    pthread_mutex_t work_mutex;       // pentru sincronizarea operatiilor efectuate de thread-uri
     queue<file_info_t> files_queue;   // stocheaza datele fisierelor care trebuie procesate de mapperi
     pthread_cond_t cond;
-    vector<map<string, vector<unsigned int>>> partial_lists; // o lista partiala contine elemente de forma (cuvant, lista de file_id-uri)
+    partial_list_t* partial_lists;    // o lista partiala contine elemente de forma (cuvant, lista de file_id-uri)
     bool finished_mapping;            // pentru a semnaliza daca procesarea de catre mapperi s-a terminat (coada de task-uri "map" este goala)
     bool finished_reducing;           // pentru a semnaliza daca procesarea de catre reduceri s-a terminat (coada de task-uri "reduce" este goala)
     bool finished_aggregation;
 } threadpool_t;
+
+typedef struct {
+    threadpool_t* threadpool;
+    unsigned int thread_id;
+} thread_struct_t;
 
 #endif // __THREADPOOL_H__
